a94dcf084307ae552429f6534b2b3c027f2f1eea
RobertoFrascona <fran_platence16@hotmail.com>
2025-07-06 18:24:26 -0300

    primer deploy

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,10 @@ import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
