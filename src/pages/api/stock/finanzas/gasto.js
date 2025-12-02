124dd64e6f41d5dcc3b6e77b4ce306ba8713aea1
franciscofrask <franciscofrascona@gmail.com>
2025-12-02 14:18:51 -0300

    borrado de apis

--- src/pages/api/stock/finanzas/gasto.js
+++
@@ -1,9 +0,0 @@
-import { db } from '@/lib/db';
-
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).json({ message: 'Método no permitido' });
-
-  const { descripcion, monto, categoria } = req.body;
-  await db.query('CALL RegistrarGastoManual(?, ?, ?)', [descripcion, monto, categoria]);
-  res.status(201).json({ message: 'Gasto registrado' });
-}
